c45d4cbb0fc700e77437e77d963ea133f406188c
g-grilli <gene@grilli.us>
2017-08-01 14:21:13 -0500

    writting data to ostgres active

--- sense_hat/sense_postgresql.py
+++ sense_hat/sense_postgresql.py
@@ -6,70 +6,62 @@ from threading import Thread
 import psycopg2
 
 try:
-    conn = psycopg2.connect(dbname='zipcode' user ='postgres' host='localhost' password='')
+    conn = psycopg2.connect("dbname='zipcode' user='postgres' host='localhost' password=''")
 
-except: print "I am unableto connect to the databse"
+except: print("I am unable to connect to the databse")
 
-curr=conn.cursor()
 TEMP_H=True
 TEMP_P=True
 HUMIDITY=True
 PRESSURE=True
-ORIENTATION=True
-ACCELERATION=True
 MAG=True
-GYRO=True
 DELAY=1800
 
-def log_data():
-    cur.execute(INSERT INTO sensor_data VALUES (sense_data));
+def log_data(tempH, tempP, hum, press, mag, dt):
+    print(tempH, tempP, hum, press, mag, dt)
+    cur = conn.cursor()
+    cur.execute("""INSERT INTO sensor_data(temp_h, temp_p, humidity, pressure, mag, datetime) 
+     VALUES (%s, %s, %s, %s, %s, %s)""", (tempH, tempP, hum, press, mag, dt ));
+    conn.commit()
     
 def get_sense_data():
-    sense_data=[]
+    sense = SenseHat()
 
     if TEMP_H:
-        sense_data.append(sense.get_temperature_from_humidity())
+        tempH = sense.get_temperature_from_humidity()
+        
 
     if TEMP_P:
-        sense_data.append(sense.get_temperature_from_pressure())
+        tempP = sense.get_temperature_from_pressure()
+        
 
     if HUMIDITY:
-        sense_data.append(sense.get_humidity())
+        hum = sense.get_humidity()
+      
 
     if PRESSURE:
-        sense_data.append(sense.get_pressure())
-
-    if ORIENTATION:
-        yaw,pitch,roll = sense.get_orientation().values()
-        sense_data.extend([pitch,roll,yaw])
+        press = sense.get_pressure()
 
     if MAG:
-        mag_x,mag_y,mag_z = sense.get_compass_raw().values()
-        sense_data.extend([mag_x,mag_y,mag_z])
+        mag = sense.get_compass()
 
-    if ACCELERATION:
-        x,y,z = sense.get_accelerometer_raw().values()
-        sense_data.extend([x,y,z])
-
-    if GYRO:
-        gyro_x,gyro_y,gyro_z = sense.get_gyroscope_raw().values()
-        sense_data.extend([gyro_x,gyro_y,gyro_z])
-
-    sense_data.append(datetime.now())
-
-    return sense_data
+    dt= datetime.now()
+    
+    return(tempH, tempP, hum, press, mag, dt)
     
 def timed_log():
     while True:
-      get_sense_data()
-      log_data()
+      datalist = get_sense_data()
+      log_data(*datalist)
       sleep(DELAY)
 
 ##### Main Program #####
+sense=SenseHat()
 while True:
     if DELAY > 0:
-        Thread(target= timed_log).start()
+        Thread(target = timed_log).start()
     
     if DELAY == 0:
-        log_data()
+        datalist = get_sense_data()
+        log_data(*datalist)
     
